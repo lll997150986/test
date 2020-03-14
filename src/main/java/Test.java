ad6efd3ddc4af226287463dbdba6b6679b1ae0af
lll997150986 <997150986@qq.com>
2020-03-14 15:06:17 +0800

    44

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -7,6 +7,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("123");
-        System.out.println("456");
+        System.out.println("123456");
     }
 }
