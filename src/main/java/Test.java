46d7540f912ce7a099fdffc8caee788bfa5e5486
lll997150986 <997150986@qq.com>
2020-03-14 14:43:40 +0800

    1

---
+++ src/main/java/Test.java
@@ -0,0 +1,11 @@
+/**
+ * @program: gitTest
+ * @description:
+ * @author: libin
+ * @create: 2020-03-14 14:37
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
